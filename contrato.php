720ae31e6de0357548f8ab4dcd829e40a52a71d4
unknown <ruizandiap.idat@gmail.com>
2022-12-01 09:25:56 -0500

    pagina-contrato

---
+++ contrato.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF=8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>HTML CONTRATO</title>
+        <link rel="stylesheet" href="css/style_cont.css">
+    </head>
+    <style>
+    body{
+        background: url(imagenes/matricula_fondo2.png);
+        background-size:cover ;
+        width: 100%;
+        height:100vh;
+    }
+    </style>
+    <body>
+
+        <form action="" class="form-box">
+            <h1 class="form-title">CONTRATO DE SERVICIOS EDUCATIVOS 2023</h1>
+        </form>
+
+    </body>
+</html>
