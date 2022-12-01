77ede1c4d532f367340dc5ce7530639f743c6e11
unknown <ruizandiap.idat@gmail.com>
2022-11-30 22:55:32 -0500

    pagina

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina01</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="contenedor-general">
+        <div class="contenedor-central">
+    
+        </div>
+        <div class="contenedor-secundario">
+            <div class="img-secun-refe">
+                <label class="titulo">Firma digital</label>
+            </div>
+            <form id="formulario_cargo" method="POST" enctype="multipart/form-data"  class="">
+                <div class="div1">
+                    <label class="negrita">Documento:</label><label class="">Contrato de servicios educativo</label>
+                </div>
+                <label class="negrita derecha">ALUMNO(A):</label>
+                <div class="div2">
+                    <div class="flex">
+                        <label class="negrita">Código:</label><label class="">12345</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Grado:</label><label class="">3 Años "A"</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Apelllido(s):</label><label class="">Perez</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Nombre(s):</label><label class="">Maria</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Fecha Nac.:</label><label class="">00/00/0000</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">DNI:</label><label class="">12345678</label>
+                    </div>
+                </div>
+                <label class="negrita derecha">APODERADO(S):</label>
+                <div class="div3"> 
+                    <div class="flex">
+                        <label class="negrita">Apelllido(s):</label><label class="">Perez</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Nombre(s):</label><label class="">Maria</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Fecha Nac.:</label><label class="">00/00/0000</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Celular:</label><label class="">999 999 999</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">DNI:</label><label class="">12345678</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Correo:</label><label class="">ejemplo@google.com</label>
+                    </div>
+                </div>
+
+                <div class="div4"> 
+                <div class="flex">
+                        <label class="negrita">Apelllido(s):</label><label class="">Perez</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Nombre(s):</label><label class="">Maria</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Fecha Nac.:</label><label class="">00/00/0000</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Celular:</label><label class="">999 999 999</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">DNI:</label><label class="">12345678</label>
+                    </div>
+                    <div class="flex">
+                        <label class="negrita">Correo:</label><label class="">ejemplo@google.com</label>
+                    </div>
+                </div>
+                <div class="img-borde-inf">
+                    <img src="img/cuadros abajo-01.png" id="img-bo2">
+                </div>
+            </form>
+            <div class="botones">
+                <button type="button" class="btn1">No soy su apoderado</button>
+                <button type="button" class="btn2">Continuar</button>
+            </div>
+        </div>
+    </div>
+</body>
