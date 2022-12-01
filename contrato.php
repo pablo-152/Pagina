e014607031960f37c6bbcbdbd1ec38c004a22d7b
John Doe <assassinsfreelol@gmail.com>
2022-12-01 11:33:54 -0500

    actu

--- contrato.php
+++ contrato.php
@@ -18,8 +18,48 @@
     <body>
 
         <form action="" class="form-box">
-            <h1 class="form-title">CONTRATO DE SERVICIOS EDUCATIVOS 2023</h1>
+            <label class="form-title">CONTRATO DE SERVICIOS EDUCATIVOS 2023</label>
+            <br>
+            <br>
+            <center><p style="font-size: 20px; text-align: justify; font-family: 'Myriad Pro', sans-serif; width: 80% ;">Conste por el presente documento el Contrato de Servicios Educa vos que celebran de una parte la empresa<label class="negrita"> L.L. JESUS MARIA S.A.C</label>
+                iden ficada con R.U.C. Nº 20600608101 con domicilio fiscal en Av. General Garzón Nº 1045 distrito de Jesús María, provincia de Lima
+                y departamento de Lima a quien en lo sucesivo se denominará EL NIDO, y de otra parte <label class="negrita">LOS PADRES DE FAMILIA</label> cuyos datos se
+                consignan en el Anexo 01 del presente contrato en los términos y condiciones siguientes:</p></center>
+
+            <center><p style="font-size: 20px; text-align: justify; font-family: 'Myriad Pro', sans-serif; width: 80% ;"><label class="negrita">CLÁUSULA PRIMERA: LAS PARTES:</label><br>
+                El NIDO es una institución educativa de gestión privada que brinda servicios educativos escolarizados en los niveles de primaria y
+                secundaria; de conformidad con su R.D. N.º 02680-2009 y Licencia de Funcionamiento emitida por la Municipalidad de Jesús María;
+                que autorizan su funcionamiento.
+                <label class="negrita">EL PADRE DE FAMILIA</label>  y/o apoderado es el Representante Legal del (los) menor (es) cuyos datos se encuentran consignados en el
+                Anexo Nº 01, en adelante LOS ESTUDIANTES, y manifiesta su voluntad de matricularlos en EL NIDO Little Leaders Pre school para el
+                Año Escolar 2023.</p></center>
+            
+            <center><p style="font-size: 20px; text-align: justify; font-family: 'Myriad Pro', sans-serif; width: 80% ;"><label class="negrita">CLÁUSULA SEGUNDA: OBJETO DEL CONTRATO:</label><br>
+                Por el presente documento, <label class="negrita">EL PADRE DE FAMILIA</label>  contrata los Servicios Educa vos ofrecidos por EL NIDO Little Leaders para la
+                enseñanza y formación de LOS ESTUDIANTES.</p></center>
+
+            <center><p style="font-size: 20px; text-align: justify; font-family: 'Myriad Pro', sans-serif; width: 80% ;"><label class="negrita">CLÁUSULA TERCERA: INFORMACIÓN BRINDADA POR EL NIDO:</label><br>
+                <label class="negrita">EL PADRE DE FAMILIA</label> declara que, desde antes de la Matrícula y de la Celebración del presente Contrato, se viene informando sobre
+                las condiciones el Horario de Clases, los servicios de apoyo al Estudiante, las ventajas y caracterís cas del Servicio Educa vo ofrecido
+                por EL NIDO a través de nuestra web site www.li leleaders.edu.pe y de nuestro Facebook Oficial.</p></center>
+
+            <center><p style="font-size: 20px; text-align: justify; font-family: 'Myriad Pro', sans-serif; width: 80% ;"><label class="negrita">CLÁUSULA CUARTA: PROPUESTA PEDAGÓGICA:</label><br>
+                La Propuesta Pedagógica de EL NIDO  ene como base el Currículo Nacional de la Educación Básica y responde al desarrollo armonioso
+                e integral de sus Estudiantes y a crear ac tudes posi vas de convivencia social, democra zación de la sociedad y ejercicio responsable
+                de la ciudadanía. Asimismo, desarrolla las metodologías ac vas que permiten el desarrollo de competencias digitales, cognitivas</p></center>
+
         </form>
+
+        <div class="botones">
+            <button type="button" class="btn2">Confirmar</button>
+
+            <input type="checkbox" id="_checkbox"><label class="label1" for="_checkbox"><div id="tick_mark"></div></label>
+            
+            <label class="text">Por favor confirme que ha leído y acepta el contrato enviado.</label>
+                    
+
+        </div>
+        
 
     </body>
 </html>
